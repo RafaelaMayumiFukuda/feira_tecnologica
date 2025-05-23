dbd7f865159651b86a8dd55d57b3adcf27bc2f43
MiguelSommerf <miguel.sommerfeldluiz@gmail.com>
2025-05-22 21:59:07 -0300

    Modificando e polindo a tela index e home.

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-header('Location: views/tela_home.php');
+require_once 'back/classes/Logout.php';
+
+$logout = new Logout();
+$logout->logout();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <title>Inicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Home</title>
   </head>
   <body
     style="
-      background-image: url('/img/bg-roxo.jpg');
+      background-image: url('assets/img/bg-roxo.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
-    "
-  >
+    ">
     <header>
       <img src="assets/img/mcm-logo.png" alt="logo">
       <h1>Inicio</h1>
@@ -25,22 +27,20 @@ header('Location: views/tela_home.php');
     <main>
       <div class="home-holder">
         <div class="home-conteiner">
-          <a href="" class="btn-1 btn"
-            ><p>Mapa</p>
-            <div class="dots-1">&gt;</div></a
-          >
-          <a href="" class="btn-1 btn"
-            ><p>Projeto</p>
-            <div class="dots-1">&gt;</div></a
-          >
-          <a href="" class="btn-2 btn"
-            ><p>ODS</p>
-            <div class="dots-2">&gt;</div></a
-          >
-          <a href="" class="btn-2 btn"
-            ><p>Créditos</p>
-            <div class="dots-2">&gt;</div></a
-          >
+          <a href="views/tela_login.php" class="btn-2 btn">
+            <p>Entrar</p>
+            <div class="dots-2">&gt;</div>
+          </a>
+
+          <a href="views/tela_cadastro.php" class="btn-2 btn">
+            <p>Cadastrar</p>
+            <div class="dots-2">&gt;</div>
+          </a>
+
+          <a href="views/tela_home.php" class="btn-3 btn">
+            <p>Anônimo</p>
+            <div class="dots-3">&gt;</div>
+          </a>
           <a href="" class="avaliar"><strong>AVALIE-NOS AQUI!</strong></a>
         </div>
       </div>
--- views/tela_home.php
+++ views/tela_home.php
@@ -1,24 +1,13 @@
 <?php
-require_once '../back/connect.php';
-
-if(session_status() === PHP_SESSION_NONE){
-  session_start();
-}
-
-#será utilizado na aba de votação
-/* if(!$_SESSION['id']){
-  echo "<script>alert('Você precisa estar logado para votar.')</script>";
-  die("Você precisa efetuar o login para votar.<br><a href='./tela_login.php'>Voltar para a tela de login</a>");
-} */
-
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../assets/css/style.css" />
-    <title>Home</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="../assets/css/style.css"/>
+    <title>Inicio</title>
   </head>
   <body
     style="
@@ -26,45 +15,53 @@ if(session_status() === PHP_SESSION_NONE){
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
-    "
-  >
+    ">
     <header>
-      <img src="../assets/img/mcm-logo.png" alt="logo" />
+      <img src="../assets/img/mcm-logo.png" alt="logo">
       <h1>Inicio</h1>
-      <img src="../assets/img/cps-logo.png" alt="logo" />
+      <img src="../assets/img/cps-logo.png" alt="logo">
     </header>
     <main>
       <div class="home-holder">
         <div class="home-conteiner">
-        <?php
-        if(@$_SESSION['id']){
-          echo "Bem-vindo " . $_SESSION['nome'] . "!";
-        }
-        ?>
-          <a href="tela_mapa.php" class="btn-1 btn"
-            ><p>Mapa</p>
-            <div class="dots-1">&gt;</div></a
-          >
-          <a href="#" class="btn-1 btn"
-            ><p>Projeto</p>
-            <div class="dots-1">&gt;</div></a
-          >
-          <a href="tela_login.php" class="btn-2 btn"
-            ><p>Entrar</p>
-            <div class="dots-2">&gt;</div></a
-          >
-          <a href="tela_cadastro.php" class="btn-2 btn"
-            ><p>Cadastrar</p>
-            <div class="dots-2">&gt;</div></a
-          >
-          <a href="../index.php" class="btn-3 btn"
-            ><p>Anônimo</p>
-            <div class="dots-3">&gt;</div></a
-          >
-          <a href="#" class="btn-3 btn"
-            ><p>Acessibilidade</p>
-            <div class="dots-3">&gt;</div></a
-          >
+          <?php
+          if(isset($_SESSION['id'])){
+            echo "Bem-vindo " . $_SESSION['nome'] . "!";
+          }
+          ?>
+          <a href="tela_mapa.php" class="btn-1 btn">
+            <p>Mapa</p>
+            <div class="dots-1">&gt;</div>
+          </a>
+
+          <a href="tela_projetos.php" class="btn-1 btn">
+            <p>Projetos</p>
+            <div class="dots-1">&gt;</div>
+          </a>
+
+          <?php if(!isset($_SESSION['id'])): ?>
+
+          <a href="tela_login.php" class="btn-2 btn">
+            <p>Entrar</p>
+            <div class="dots-2">&gt;</div>
+          </a>
+
+          <a href="tela_cadastro.php" class="btn-2 btn">
+            <p>Cadastrar</p>
+            <div class="dots-2">&gt;</div>
+          </a>
+
+          <?php endif ?>
+
+          <a href="" class="btn-2 btn">
+            <p>ODS</p>
+            <div class="dots-2">&gt;</div>
+          </a>
+
+          <a href="" class="btn-2 btn">
+            <p>Créditos</p>
+            <div class="dots-2">&gt;</div>
+          </a>
           <a href="" class="avaliar"><strong>AVALIE-NOS AQUI!</strong></a>
         </div>
       </div>
