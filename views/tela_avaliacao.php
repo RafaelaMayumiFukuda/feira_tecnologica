2784d120166f89a98e48cd78996415001ff3377d
Lívia Amaral <125597286+Liviaamaralsales@users.noreply.github.com>
2025-08-06 15:42:01 -0300

    Update tela_avaliacao.php

--- views/tela_avaliacao.php
+++ views/tela_avaliacao.php
@@ -12,79 +12,89 @@ if (session_status() === PHP_SESSION_NONE) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/avaliacao.css">
+    <link rel="stylesheet" href="/assets/css/TelaAvaliacao.css">
 </head>
-<body class="telaAvaliacao">
-    <header>
+
+  <body class="telaAvaliacao">
+    <header style="display: flex; background-color: aqua;">
+        <div class="menu-toggle" id="mobile-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+
+        <div class="logo-container">
+            <img src="/assets/img/etecmcm.png" alt="Logo MCM" />
+        </div>
         <div class="ORGInfoHeader">
             <h1>Avaliação</h1>
         </div>
-        <button class="btn-voltar" onclick="history.back()">Voltar</button>
     </header>
 
     <main>
-        <div class="logo-container">
-            <img src="../assets/img/etecmcm.png" alt="Logo MCM" />
-        </div>
-        
+    
         <div class="avaliacao-container">
-            <div class="avaliacao-header">
-                <img src="" alt="" class="avaliacao-logo"> <!-- Inserir Foto-->
-                <div class="avaliacao-infos">
-                    <div class="campo">
-                        <input type="text" placeholder="Projeto:" readonly/>
-                    </div>
-                    <div class="campo">
-                        <input type="text" placeholder="Alunos:" readonly/>
-                    </div>
-                    <div class="campo">
-                        <input type="text" placeholder="Turma:" readonly/>
-                    </div>
-                </div>
+    <div class="avaliacao-header">
+        <img src="" alt="" class="avaliacao-logo"> <!-- Inserir Foto-->
+
+        <div class="avaliacao-infos">
+            <div class="campo">
+                <input type="text" placeholder="Projeto:" readonly/>
             </div>
-            <div class="avaliacao-linha">
-                <span>Apresentação</span>
-                <div class="avaliacao-estrelas-container">
-                    <div class="avaliacao-estrelas" data-id="0"></div>
-                </div>
+            <div class="campo">
+                <input type="text" placeholder="Alunos:" readonly/>
             </div>
-            <div class="avaliacao-linha">
-                <span>Organização</span>
-                <div class="avaliacao-estrelas-container">
-                    <div class="avaliacao-estrelas" data-id="1"></div>
-                </div>
-            </div>
-            <div class="avaliacao-linha">
-                <span>Banner</span>
-                <div class="avaliacao-estrelas-container">
-                    <div class="avaliacao-estrelas" data-id="2"></div>
-                </div>
-            </div>
-            <div class="avaliacao-linha">
-                <span>Ideia do projeto</span>
-                <div class="avaliacao-estrelas-container">
-                    <div class="avaliacao-estrelas" data-id="3"></div>
-                </div>
+            <div class="campo">
+                <input type="text" placeholder="Turma:" readonly/>
             </div>
         </div>
-        <div class="avaliacao-comentario">
-            <textarea class="avaliacao-textarea" placeholder="Deixe um comentário..."></textarea>
-            <button class="avaliacao-button" id="">Enviar</button>
+    </div>
+
+    <div class="avaliacao-linha">
+        <span>Apresentação</span>
+        <div class="avaliacao-estrelas-container">
+        <div class="avaliacao-estrelas" data-id="0"></div>
         </div>
+    </div>
+
+    <div class="avaliacao-linha">
+        <span>Organização</span>
+        <div class="avaliacao-estrelas-container">
+        <div class="avaliacao-estrelas" data-id="1"></div>
+        </div>
+    </div>
+
+    <div class="avaliacao-linha">
+        <span>Banner</span>
+        <div class="avaliacao-estrelas-container">
+        <div class="avaliacao-estrelas" data-id="2"></div>
+        </div>
+    </div>
+
+    <div class="avaliacao-linha">
+        <span>Ideia do projeto</span>
+        <div class="avaliacao-estrelas-container">
+        <div class="avaliacao-estrelas" data-id="3"></div>
+        </div>
+    </div>
+    </div>
+
+    <div class="avaliacao-comentario">
+    <textarea class="avaliacao-textarea" placeholder="Deixe um comentário..."></textarea>
+    <button class="avaliacao-button" id="">Enviar</button>
+    </div>
+
     </main>
 
     <div id="mySideMenu" class="side-menu">
       <a href="javascript:void(0)" class="close-btn" onclick="closeMenu()">&times;</a>
-      <a href="tela_mapa.php">Mapa</a>
-      <a href="tela_avaliacao.php">Avaliação</a>
-      <a href="tela_projetos.php">Projetos</a>
-      <a href="tela_ranking.php">Ranking</a>
-      <a href="tela_cursos.php">Cursos</a>
-      <a href="tela_sobreEtec.php">Sobre a Etec</a>
-      <a href="tela_acessibilidade.php">Acessibilidade</a>
-      <?php if(isset($_SESSION['id'])): ?>
-      <a href="../back/logout.php" class="deslogar" id="deslogar" name="deslogar">Sair da Conta</a>
-      <?php endif; ?>
+      <a href="#">Mapa</a>
+      <a href="TelaAvaliacao.html">Avaliação</a>
+      <a href="TelaProjetos.html">Projetos</a>
+      <a href="TelaRanking.html">Ranking</a>
+      <a href="TelaCursos.html">Cursos</a>
+      <a href="TelaSobreEtec.html">Sobre a Etec</a>
+      <a href="TelaConfiguracoes.html">Configurações</a>
     </div>
 
     <script>
