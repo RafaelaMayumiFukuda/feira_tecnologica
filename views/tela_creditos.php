ac937e828a1cf4f15949c473a9e08b2c988ddb64
Lívia Amaral <125597286+Liviaamaralsales@users.noreply.github.com>
2025-08-08 17:21:12 -0300

    Update tela_creditos.php

--- views/tela_creditos.php
+++ views/tela_creditos.php
@@ -37,20 +37,20 @@ if (session_status() === PHP_SESSION_NONE) {
             <div class="cartoes">
                 <?php
                     $dados = [
-                        ["Ângelo Gabriel", "/imagens", "Team leader - Back", null, "https://github.com/projAngeloAraujo"],
-                        ["Enzo Móbile", "/imagens", "Programador - Back", null, "https://github.com/enzomobile"],
-                        ["Guilherme Solon", "/imagens", "Programador - Back", "https://www.linkedin.com/in/guilherme-solon-6b4a142a9/", "https://github.com/Solonguitec"],
-                        ["Gustavo da Rocha", "/imagens", "Programador - Back", null, "https://github.com/gustapinheiro"],
-                        ["Gustavo Rangel", "/imagens", "Programador - Back", null, "https://github.com/DEVRangelll"],
-                        ["Iuri Carati", "/imagens", "Programador - Back", null, "https://github.com/ToxicDumpster"],
-                        ["Jean Marcos", "/imagens", "Programador - Back", null, "https://github.com/jean666"],
-                        ["Júlia Medeiros", "/imagens", "Programadora - Back", null, "https://github.com/jumedeirost"],
-                        ["Matheus Pereira", "/imagens", "Programador - Back", null, "https://github.com/MatheusSontos"],
-                        ["Miguel Sommerfeld", "/imagens", "Team leader - Back", "https://www.linkedin.com/in/miguel-sommerfeld-06491b340/", "https://github.com/MiguelSommerf"],
-                        ["Miguel Teodoro", "/imagens", "Programador - Back", null, "https://github.com/Miguelteodorodesouza"],
-                        ["Sabrina Bela", "/imagens", "Programadora - Back", null, "https://github.com/sabrinabela1"],
-                        ["Stephany dos Santos", "/imagens", "Programadora - Back", null, "https://github.com/stephanydossantos16"],
-                        ["Thomas Coradi", "/imagens", "Programador - Back", null, "https://github.com/thomcoradi"]
+                        ["Ângelo Gabriel", "../assets/img/Perfil.jpeg", "Team leader - Back", null, "https://github.com/projAngeloAraujo"],
+                        ["Enzo Móbile", "../assets/img/Perfil.jpeg", "Programador - Back", null, "https://github.com/enzomobile"],
+                        ["Guilherme Solon", "../assets/img/Back/Guilherme Solon.jpeg", "Programador - Back", "https://www.linkedin.com/in/guilherme-solon-6b4a142a9/", "https://github.com/Solonguitec"],
+                        ["Gustavo da Rocha", "../assets/img/Perfil.jpeg", "Programador - Back", null, "https://github.com/gustapinheiro"],
+                        ["Gustavo Rangel", "../assets/img/Back/Gustavo Rangel.jpeg", "Programador - Back", null, "https://github.com/DEVRangelll"],
+                        ["Iuri Carati", "../assets/img/Perfil.jpeg", "Programador - Back", null, "https://github.com/ToxicDumpster"],
+                        ["Jean Marcos", "../assets/img/Back/Jean.jpeg", "Programador - Back", null, "https://github.com/jean666"],
+                        ["Júlia Medeiros", "../assets/img/Perfil.jpeg", "Programadora - Back", null, "https://github.com/jumedeirost"],
+                        ["Matheus Pereira", "../assets/img/Back/Matheus.jpeg", "Programador - Back", null, "https://github.com/MatheusSontos"],
+                        ["Miguel Sommerfeld", "../assets/img/Back/Miguel Sommerfeld.jpeg", "Team leader - Back", "https://www.linkedin.com/in/miguel-sommerfeld-06491b340/", "https://github.com/MiguelSommerf"],
+                        ["Miguel Teodoro", "../assets/img/Back/Miguel Teodoro_.jpg", "Programador - Back", null, "https://github.com/Miguelteodorodesouza"],
+                        ["Sabrina Bela", "../assets/img/Back/Sabrina Nicole.jpeg    ", "Programadora - Back", null, "https://github.com/sabrinabela1"],
+                        ["Stephany dos Santos", "../assets/img/Back/Stephany Santos.jpeg", "Programadora - Back", null, "https://github.com/stephanydossantos16"],
+                        ["Thomas Coradi", "../assets/img/Back/Thomas.jpeg", "Programador - Back", null, "https://github.com/thomcoradi"]
                     ];
 
                     foreach ($dados as $pessoa) {
@@ -73,14 +73,14 @@ if (session_status() === PHP_SESSION_NONE) {
             <div class="cartoes">
                 <?php
                     $dadosBanco = [
-                        ["Amanda Rodriguez", "/imagens", "Administradora - Banco de Dados", "", "https://github.com/amandszr"],
-                        ["Bryan de Oliveira", "/imagens", "Administrador - Banco de Dados", "", "https://github.com/BryanOli17"],
-                        ["Denner Pereira", "/imagens", "Administrador - Banco de Dados", "", "https://github.com/Denner106"],
-                        ["Guilherme Benatte", "/imagens", "Team leader - Banco de Dados", "", "https://github.com/guibenatte"],
-                        ["Katharina Iaussoghi", "/imagens", "Administradora - Banco de Dados", "", "https://github.com/Katharinasilveira"],
-                        ["Mariana Campello", "/imagens", "Administradora - Banco de Dados", "https://www.linkedin.com/in/mariana-cunha-campello-b865b5363/", "https://github.com/marianacampelo"],
-                        ["Nicole Pereira", "/imagens", "Administradora - Banco de Dados", "", "https://github.com/Nicolepereiragregorutti"],
-                        ["Rafaela Mayumi", "/imagens", "Team leader - Banco de Dados", "", "https://github.com/RafaelaMayumiFukuda"]
+                        ["Amanda Rodriguez", "../assets/img/Banco de dados/Amanda Rodrigues_.jpg", "Administradora - Banco de Dados", "", "https://github.com/amandszr"],
+                        ["Bryan de Oliveira", "../assets/img/Banco de dados/Bryan.jpeg", "Administrador - Banco de Dados", "", "https://github.com/BryanOli17"],
+                        ["Denner Pereira", "../assets/img/Banco de dados/Denner.jpeg", "Administrador - Banco de Dados", "", "https://github.com/Denner106"],
+                        ["Guilherme Benatte", "../assets/img/Banco de dados/Guilherme Benatte.jpeg", "Team leader - Banco de Dados", "", "https://github.com/guibenatte"],
+                        ["Katharina Iaussoghi", "../assets/img/Banco de dados/Katharina.jpeg", "Administradora - Banco de Dados", "", "https://github.com/Katharinasilveira"],
+                        ["Mariana Campello", "../assets/img/Banco de dados/Mariana.jpeg", "Administradora - Banco de Dados", "https://www.linkedin.com/in/mariana-cunha-campello-b865b5363/", "https://github.com/marianacampelo"],
+                        ["Nicole Pereira", "../assets/img/Perfil.jpeg", "Administradora - Banco de Dados", "", "https://github.com/Nicolepereiragregorutti"],
+                        ["Rafaela Mayumi", "../assets/img/Banco de dados/Rafaela.jpeg", "Team leader - Banco de Dados", "", "https://github.com/RafaelaMayumiFukuda"]
                     ];
 
                     foreach ($dadosBanco as $pessoa) {
@@ -103,19 +103,19 @@ if (session_status() === PHP_SESSION_NONE) {
             <div class="cartoes">
                 <?php
                     $dadosFront = [
-                        ["Amanda Kaori", "/imagens", "Programadora - Front", null, "https://github.com/projamandakaori"],
-                        ["Camila Vitoria", "/imagens", "Programadora - Front", null, "https://github.com/ProjCamilaVitoria"],
-                        ["Cauã Arthur", "/imagens", "Programador - Front", null, "https://github.com/Projcauaramos"],
-                        ["Cecília Santiago", "/imagens", "Programadora - Front", null, "https://github.com/ceciliasf"],
-                        ["Eduardo de Ataíde", "/imagens", "Programador - Front", null, "https://github.com/duduusxz"],
-                        ["Giulia Benedetti", "/imagens", "Team leader - Front", null, "https://github.com/projgioliveira"],
-                        ["João Xavier", "/imagens", "Programador - Front", "https://www.linkedin.com/in/jo%C3%A3o-vitor-xavier-de-carvalho-469147183/?trk=opento_nprofile_details", "https://github.com/joaovitorxc"],
-                        ["Kevin Rafael", "/imagens", "Programador - Front", null, "https://github.com/Kevin2007x"],
-                        ["Lívia Amaral", "/imagens", "Team leader - Front", "https://www.linkedin.com/in/l%C3%ADvia-amaral-sales-antonio-675219326/", "https://github.com/Liviaamaralsales"],
-                        ["Olavo Alves", "/imagens", "Programador - Front", null, "https://github.com/Olavoschiavi"],
-                        ["Sabrina Vitória", "/imagens", "Programadora - Front", null, "https://github.com/Sabrinavmoura"],
-                        ["Stefanny Sayuri", "/imagens", "Programadora - Front", null, "https://github.com/StefannySayuri"],
-                        ["Welington Fernando", "/imagens", "Programador - Front", null, "https://github.com/Welingtonf"]
+                        ["Amanda Kaori", "../assets/img/Front/Amanda Kaori.jpg", "Programadora - Front", null, "https://github.com/projamandakaori"],
+                        ["Camila Vitoria", "../assets/img/Front/Camila.jpeg", "Programadora - Front", null, "https://github.com/ProjCamilaVitoria"],
+                        ["Cauã Arthur", "../assets/img/Front/Cauã.jpeg", "Programador - Front", null, "https://github.com/Projcauaramos"],
+                        ["Cecília Santiago", "../assets/img/Front/Cecilia.jpeg", "Programadora - Front", null, "https://github.com/ceciliasf"],
+                        ["Eduardo de Ataíde", "../assets/img/Front/Eduardo.jpeg", "Programador - Front", null, "https://github.com/duduusxz"],
+                        ["Giulia Benedetti", "../assets/img/Front/Giulia.jpeg", "Team leader - Front", null, "https://github.com/projgioliveira"],
+                        ["João Xavier", "../assets/img/Front/João.jpeg", "Programador - Front", "https://www.linkedin.com/in/jo%C3%A3o-vitor-xavier-de-carvalho-469147183/?trk=opento_nprofile_details", "https://github.com/joaovitorxc"],
+                        ["Kevin Rafael", "../assets/img/Front/Kevin.jpeg", "Programador - Front", null, "https://github.com/Kevin2007x"],
+                        ["Lívia Amaral", "../assets/img/Front/Livia.jpeg", "Team leader - Front", "https://www.linkedin.com/in/l%C3%ADvia-amaral-sales-antonio-675219326/", "https://github.com/Liviaamaralsales"],
+                        ["Olavo Alves", "../assets/img/Front/Olavo.jpeg", "Programador - Front", null, "https://github.com/Olavoschiavi"],
+                        ["Sabrina Vitória", "../assets/img/Front/Sabrina Vitoria.jpeg", "Programadora - Front", null, "https://github.com/Sabrinavmoura"],
+                        ["Stefanny Sayuri", "../assets/img/Front/Stefanie Sayuri.jpeg", "Programadora - Front", null, "https://github.com/StefannySayuri"],
+                        ["Welington Fernando", "../assets/img/Front/Wellington.jpeg", "Programador - Front", null, "https://github.com/Welingtonf"]
                     ];
 
                     foreach ($dadosFront as $pessoa) {
@@ -138,10 +138,10 @@ if (session_status() === PHP_SESSION_NONE) {
             <div class="cartoes">
                 <?php
                     $dadosGestao = [
-                        ["Giovana Pracanico", "/imagem", "Gestão", null, "https://github.com/projgipracanico"],
-                        ["Heitor Albuquerque", "/imagens", "gestor", null, "https://github.com/projheitorfreitas"],
-                        ["Joshua Rodrigues", "/imagens", "gestor", null, "https://github.com/JoshRodriguescae"],
-                        ["Luara Gouveia", "/imagens", "gestora", null, "https://github.com/luarag45"]
+                        ["Giovana Pracanico", "../assets/img/Gestão/Giovana.jpeg", "Gestora", null, "https://github.com/projgipracanico"],
+                        ["Heitor Albuquerque", "../assets/img/Gestão/Heitor.jpeg", "Gestor", null, "https://github.com/projheitorfreitas"],
+                        ["Joshua Rodrigues", "../assets/img/Gestão/Joshua.jpeg", "Gestor", null, "https://github.com/JoshRodriguescae"],
+                        ["Luara Gouveia", "../assets/img/Gestão/Luara.jpeg", "Gestora", null, "https://github.com/luarag45"]
                     ];
                     foreach ($dadosGestao as $pessoa) {
                         echo "<div class='cartao'>";
@@ -163,8 +163,8 @@ if (session_status() === PHP_SESSION_NONE) {
             <div class="cartoes">
                 <?php
                     $dadosOrientadores = [
-                        ["Ricardo Moreira", "/imagens", "Orientador do projeto", null, "https://github.com/prof-ricardo"],
-                        ["Edilma Bindá", "/imagens", "Orientadora do projeto", null, "https://github.com/edilmabinda"]
+                        ["Ricardo Moreira", "../assets/img/Orientadores/Ricardo.jpeg", "Orientador do projeto", null, "https://github.com/prof-ricardo"],
+                        ["Edilma Bindá", "../assets/img/Orientadores/Edilma.jpeg", "Orientadora do projeto", null, "https://github.com/edilmabinda"]
                     ];
                     foreach ($dadosOrientadores as $pessoa) {
                         echo "<div class='cartao'>";
